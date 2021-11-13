6b214efe0a6711058d3586ef3f003accdfba1014
Srinivas Akella <srinivas72075@gmail.com>
2021-11-13 12:59:40 +0000

    This is the mobx code works similarly as redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { observer, useObservable } from "mobx-react-lite";
 
-function App() {
+import "./App.css";
+
+/* This is a simple example but when you use multiple pages it will be more useful */
+const App = observer(() => {
+  const store = useObservable({
+    count: 1,
+    addOne() {
+      store.count++;
+      /* unlike redux without creating a copy of the state, 
+      we can directly modify the state here. */
+    },
+    minusOne() {
+      store.count--;
+    },
+  });
+
+  function add() {
+    store.addOne();
+    // we can directly access the methods of store
+  }
+  function minus() {
+    store.minusOne();
+    // we can directly access the methods of store
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="app-head">Count: {store.count}</h1>
+        <button onClick={add}>Add</button>
+        <button onClick={minus}>Minus</button>
       </header>
     </div>
   );
-}
+});
 
 export default App;
